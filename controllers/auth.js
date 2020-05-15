b1aa69b77253fd163d1d84dea166b89d9cb42f68
samuelhhawkins <samuel.h.hawkins@hotmail.com>
2020-05-15 09:13:18 -0700

    signup route created

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 let db = require('../models')
 let router = require('express').Router()
+let jwt = require('jsonwebtoken')
+
 
 // POST /auth/login (find and validate user; send token)
 router.post('/login', (req, res) => {
@@ -11,7 +13,38 @@ router.post('/login', (req, res) => {
 // POST to /auth/signup (create user; generate token)
 router.post('/signup', (req, res) => {
   console.log(req.body)
-  res.send('STUB POST /auth/signup')
+  // Look up the user mayne by email to make sure they are actually new
+  db.User.findOne({email: req.body.email })
+  .then(user=> {
+    // If user already exists dont let them create another account
+    if (user){
+      //No no! signup instead
+      returnres.status(409).send({ message: 'Email address in use'})
+    }
+
+    // we know the user is new and legit! CReate them
+    db.User.create(req.body)
+    .then(newUser => {
+      //YAYA things worked and users exist now create a token for em
+      let token = jwt.sign(newUser.toJSON(), process.env.JWT_SECRET,{
+        expiresIn:120 // 60 * 60 * 8 // 8 hours in seconds
+      })
+      res.send({ token })
+    })
+    .catch(innerErr => {
+      console.log('Error creating user', innerErr);
+      if(innerErr.name === "validationError"){
+        res.status(412).send({message: `validation Error ${innerErr.message}`})
+      }
+      else{
+        res.status(500).send({message: 'Error creating user'})
+      }
+    })
+  })
+  .catch(err => {
+    console.log('ERROR IN YA POST AUTH SIGNUP', err)
+    res.status(503).send({message: 'Database or Server errors' })
+  })
 })
 
 // NOTE: User should be logged in to access this route
